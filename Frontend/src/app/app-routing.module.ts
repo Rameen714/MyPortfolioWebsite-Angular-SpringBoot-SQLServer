ff65015f7deaa2ecefc5cb124c55a5db429d7279
Rameen714 <rameen.amir.malik@gmail.com>
2023-07-27 12:33:50 +0500

    Login and Add project components added

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { LoginComponent } from './login/login.component';
+import { AddProjectComponent } from './add-project/add-project.component';
 
 const routes: Routes = [
   
   { path: '', component: HomeComponent},
   { path: 'my-projects',component: MyProjectsComponent},
   { path: 'contact-me', component: ContactMeComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'add', component: AddProjectComponent},
 ];
 
 @NgModule({
@@ -17,4 +21,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const RoutingComponent = [ HomeComponent, MyProjectsComponent, ContactMeComponent];
+export const RoutingComponent = [ HomeComponent, MyProjectsComponent, ContactMeComponent, LoginComponent];
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,15 +6,20 @@ import { AppComponent } from './app.component';
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
 import { ScrollDispatchModule} from '@angular/cdk/scrolling';
 import { DragDropModule} from '@angular/cdk/drag-drop';
-import { MatMenu,MatButton, MatSidenav,MatSlideToggleModule,MatCard, MatCardModule, MatToolbarModule, MatIconModule, MatGridListModule } from '@angular/material';
+import { MatMenu,MatButton, MatSidenav,MatSlideToggleModule,MatCard, MatCardModule, MatToolbarModule, MatIconModule, MatGridListModule,MatFormFieldModule, MatInputModule, MatButtonModule } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { LoginComponent } from './login/login.component';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { AddProjectComponent } from './add-project/add-project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewCmpComponent,
     RoutingComponent,
+    LoginComponent,
+    AddProjectComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,6 +30,11 @@ import { Observable } from 'rxjs';
     MatIconModule,
     HttpClientModule,
     MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    
+
 
   ],
   providers: [],
